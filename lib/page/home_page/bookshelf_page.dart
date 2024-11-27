eee2fa06a9fc24bb9162289cc55fe927a0e5f910
huangxulei <pjhxl@qq.com>
2024-11-27 18:03:00 +0800

    1127

--- lib/page/home_page/bookshelf_page.dart
+++ lib/page/home_page/bookshelf_page.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hreader/utils/get_path/cache_path.dart';
 import 'package:hreader/utils/log/common.dart';
+import 'package:hreader/utils/toast/common.dart';
 
 class BookshelfPage extends ConsumerStatefulWidget {
   const BookshelfPage({super.key});
@@ -28,6 +30,84 @@ class BookshelfPageState extends ConsumerState<BookshelfPage>
     List<PlatformFile> files = result.files;
     HLog.info('importBook files: ${files.toString()}');
     List<File> fileList = [];
+
+    if (!Platform.isAndroid) {
+      fileList = await Future.wait(files.map((file) async {
+        Directory tempDir = await getAnxCacheDir();
+        File tempFile = File("${tempDir.path}/${file.name}");
+        await File(file.path!).copy(tempFile.path);
+        return tempFile;
+      }).toList());
+    } else {
+      fileList = files.map((file) => File(file.path!)).toList();
+    }
+
+    HLog.info("importBook fileList: ${fileList.toString()}");
+
+    List<File> supportedFiles = fileList.where((file) {
+      return allowBookExtensions.contains(file.path.split('.').last);
+    }).toList();
+    List<File> unsupportedFiles = fileList.where((file) {
+      return !allowBookExtensions.contains(file.path.split('.').last);
+    }).toList();
+
+    // delete unsupported files
+    for (var file in unsupportedFiles) {
+      file.deleteSync();
+    }
+
+    Widget bookItem(String path, Icon icon) {
+      return Row(children: [
+        icon,
+        Expanded(
+            child: Text(path.split('/').last,
+                style: const TextStyle(
+                    fontWeight: FontWeight.w300,
+                    overflow: TextOverflow.ellipsis)))
+      ]);
+    }
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("${fileList.length}"),
+            content: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(allowBookExtensions.join(' / ')),
+                  const SizedBox(height: 10),
+                  if (unsupportedFiles.isNotEmpty)
+                    Text("${unsupportedFiles.length}"),
+                  const SizedBox(height: 20),
+                  for (var file in unsupportedFiles)
+                    bookItem(file.path, const Icon(Icons.error)),
+                  for (var file in supportedFiles)
+                    bookItem(file.path, const Icon(Icons.done)),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    for (var file in supportedFiles) {
+                      file.deleteSync();
+                    }
+                  },
+                  child: Text("取消")),
+              if (supportedFiles.isNotEmpty)
+                TextButton(
+                    onPressed: () async {
+                      for (var file in supportedFiles) {
+                        HToast.show(file.path.split('/').last);
+                      }
+                    },
+                    child: Text("导入${supportedFiles.length}本书"))
+            ],
+          );
+        });
   }
 
   @override
---
+++ lib/utils/toast/common.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:hreader/main.dart';
+
+class HToast {
+  static FToast fToast = FToast();
+
+  static void init(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      fToast.init(context);
+    });
+  }
+
+  static void show(String message, {Icon? icon, int duration = 2000}) {
+    Widget toast = Container(
+      // width: MediaQuery.of(navigatorKey.currentContext!).size.width * 0.1,
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Theme.of(navigatorKey.currentContext!).colorScheme.surface,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          icon ?? const Icon(Icons.info_outline),
+          const SizedBox(
+            width: 12.0,
+          ),
+          Flexible(
+            child: Text(
+              message,
+              // wrap
+              style: TextStyle(
+                color: Theme.of(navigatorKey.currentContext!)
+                    .colorScheme
+                    .onSurface,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    // close previous toast
+    fToast.removeQueuedCustomToasts();
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.BOTTOM,
+      toastDuration: Duration(milliseconds: duration),
+    );
+
+    // // Custom Toast Position
+    // fToast.showToast(
+    //     child: toast,
+    //     toastDuration: Duration(seconds: 2),
+    //     positionedToastBuilder: (context, child) {
+    //       return Positioned(
+    //         child: child,
+    //         top: 16.0,
+    //         left: 16.0,
+    //       );
+    //     });
+  }
+}
